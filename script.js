a7d1c3e56135164f2b9c85de6e62bab155f0ee03
orvalho <rasa396@gmail.com>
2018-05-23 23:27:59 +0300

    #4

--- script.js
+++ script.js
@@ -27,3 +27,13 @@ function isOldEnoughToDrive(age) {
 function isOldEnoughToVote(age) {
   return age >= 18 ? true : false;
 }
+
+// 4. isOldEnoughToDrinkAndDrive
+// Write a function called "isOldEnoughToDrinkAndDrive".
+// Given a number, in this case an age, "isOldEnoughToDrinkAndDrive" returns whether a person of this given age is old enough
+// to legally drink and drive in the United States.
+// Notes:* The legal drinking age in the United States is 21.* It is always illegal to drink and drive in the United States.
+
+function isOldEnoughToDrinkAndDrive(age) {
+  return false;
+}
