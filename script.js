8ca192fc70a57e9fb80f241c242d4951d3b9401b
orvalho <rasa396@gmail.com>
2018-05-23 23:21:49 +0300

    #2

--- script.js
+++ script.js
@@ -8,4 +8,12 @@ function isOldEnoughToDrink(age) {
   return age >= 21 ? true : false;
 }
 
-// 2.
+// 2. isOldEnoughToDrive
+// Write a function called "isOldEnoughToDrive".
+// Given a number, in this case an age, "isOldEnoughToDrive" returns whether a person of this given age is old enough
+// to legally drive in the United States.
+// Notes:* The legal driving age in the United States is 16.
+
+function isOldEnoughToDrive(age) {
+  return age >= 16 ? true : false;
+}
