9d6cc9c325d9e4cc573adb2e3bdea565431716bc
orvalho <rasa396@gmail.com>
2018-05-23 23:06:20 +0300

    #1

---
+++ script.js
@@ -0,0 +1,11 @@
+// 1. isOldEnoughToDrink
+// Write a function called "isOldEnoughToDrink".
+// Given a number, in this case an age, "isOldEnoughToDrink" returns whether a person of this given age is old enough
+// to legally drink in the United States.
+// Notes:* The legal drinking age in the United States is 21.
+
+function isOldEnoughToDrink(age) {
+  return age >= 21 ? true : false;
+}
+
+// 2.
