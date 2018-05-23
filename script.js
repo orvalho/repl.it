92be91364f455bdb3ce1edfbfda07d589cc781c4
orvalho <rasa396@gmail.com>
2018-05-23 23:30:46 +0300

    #5

--- script.js
+++ script.js
@@ -37,3 +37,12 @@ function isOldEnoughToVote(age) {
 function isOldEnoughToDrinkAndDrive(age) {
   return false;
 }
+
+// 5. getProperty
+// Write a function called "getProperty".
+// Given an object and a key, "getProperty" returns the value of the property at the given key. 
+// Notes:* If there is no property at the given key, it should return undefined.
+
+function getProperty(obj, key) {
+  return obj.key;
+}
