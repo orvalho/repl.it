90a2ddebba2171e9260c31a20f855ba6dbd87fd1
orvalho <rasa396@gmail.com>
2018-05-24 22:54:51 +0300

    #8

--- script.js
+++ script.js
@@ -63,3 +63,17 @@ function addProperty(obj, key) {
 function removeProperty(obj, key) {
   delete obj[key];
 }
+
+// 8. checkAge
+// Write a function called "checkAge". 
+// Given a person's name and age, "checkAge" returns one of two messages:
+// "Go home, {insert_name_here}!", if they are younger than 21."Welcome, {insert_name_here}!", if they are 21 or older.
+// Naturally, replace "{insert_name_here}" with the given name. :)
+
+function checkAge(name, age) {
+  if(age < 21) {
+    return "Go home, " + name + "!";
+  } else {
+    return "Welcome, " + name + "!";
+  }
+}
