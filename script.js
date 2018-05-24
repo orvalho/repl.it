ed880f5331915cd1d085d10940c70b9ab0293501
orvalho <rasa396@gmail.com>
2018-05-24 22:56:55 +0300

    #9

--- script.js
+++ script.js
@@ -77,3 +77,12 @@ function checkAge(name, age) {
     return "Welcome, " + name + "!";
   }
 }
+
+// 9. getFullName
+// Write a function called "getFullName".
+// Given a first and a last name, "getFullName" returns a single string with the given first and last names separated
+// by a single space.
+
+function getFullName(firstName, lastName) {
+  return firstName + " " + lastName;
+}
