c33ef6cf6d016081070b9727da7a676aa13ca87b
orvalho <rasa396@gmail.com>
2018-05-25 21:01:03 +0300

    #20

--- script.js
+++ script.js
@@ -166,3 +166,15 @@ function isOdd(num) {
 function isSameLength(word1, word2) {
   return word1.length === word2.length ? true : false;
 }
+
+// 20. areBothOdd
+// Write a function called "areBothOdd".
+// Given 2 numbers, "areBothOdd" returns whether or not both of the given numbers are odd.
+
+function areBothOdd(num1, num2) {
+  if (num1 % 2 !== 0 && num2 % 2 !== 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
