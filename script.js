3740ecf9906bbe50c404abbaa408d0698d296be0
orvalho <rasa396@gmail.com>
2018-05-26 19:39:50 +0300

    #24

--- script.js
+++ script.js
@@ -206,3 +206,11 @@ function isOddLength(word) {
 function isEvenLength(word) {
   return word.length % 2 === 0 ? true: false;
 }
+
+// 24. isEvenAndGreaterThanTen
+// Write a function called "isEvenAndGreaterThanTen".
+// Given a number, "isEvenAndGreaterThanTen" returns whether it is both even and greater than 10.
+
+function isEvenAndGreaterThanTen(num) {
+  return num % 2 === 0 && num > 10 ? true : false;
+}
