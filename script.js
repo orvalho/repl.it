41dbcf8ff0f6886dca45bac5496ed136d2aa0e7b
orvalho <rasa396@gmail.com>
2018-05-27 00:13:10 +0300

    #32

--- script.js
+++ script.js
@@ -272,3 +272,14 @@ function addFullNameProperty(obj) {
 function addObjectProperty(obj1, key, obj2) {
   obj1[key] = obj2;
 }
+
+// 32. isPersonOldEnoughToDrinkAndDrive
+// Write a function called "isPersonOldEnoughToDrinkAndDrive".
+// Given a "person" object, that contains an "age" property, "isPersonOldEnoughToDrinkAndDrive" returns
+// whether the given person is old enough to legally drink and drive in the United States.
+// Notes:* The legal drinking age in the United States is 21.* The legal driving age in the United States
+// is 16.* It is always illegal to drink and drive in the United States.
+
+function isPersonOldEnoughToDrinkAndDrive(person) {
+  return false;
+}
