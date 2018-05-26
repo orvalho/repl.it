44233a6c59b054c3cf405459ff76f1014aa88326
orvalho <rasa396@gmail.com>
2018-05-27 00:16:27 +0300

    #33

--- script.js
+++ script.js
@@ -283,3 +283,13 @@ function addObjectProperty(obj1, key, obj2) {
 function isPersonOldEnoughToDrinkAndDrive(person) {
   return false;
 }
+
+// 33. isPersonOldEnoughToDrive
+// Write a function called "isPersonOldEnoughToDrive".
+// Given a "person" object, that contains an "age" property, "isPersonOldEnoughToDrive" returns whether
+// the given person is old enough to drive.
+// Notes:* The legal driving age in the United States is 16.
+
+function isPersonOldEnoughToDrive(person) {
+  return person.age >= 16 ? true : false;
+}
