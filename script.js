6195bfddb0bf4937e2edccf5ac7e44c9654db692
orvalho <rasa396@gmail.com>
2018-06-03 23:13:06 +0300

    #39

--- script.js
+++ script.js
@@ -343,3 +343,11 @@ function addToBack(arr, element) {
   arr.push(element);
   return arr;
 }
+
+// 39. computeAreaOfARectangle
+// Write a function called "computeAreaOfARectangle".
+// Given the length and width of a rectangle, "computeAreaOfARectangle" returns its area.
+
+function computeAreaOfARectangle(length, width) {
+  return length * width;
+}
