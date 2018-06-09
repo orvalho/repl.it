16dbd0de4f2a81aec0e9b871bd41bb05c9f41f02
orvalho <rasa396@gmail.com>
2018-06-09 20:25:20 +0300

    #54

--- script.js
+++ script.js
@@ -467,3 +467,13 @@ function getAllElementsButLast(array) {
   array.pop();
   return array;
 }
+
+// 54. removeFromFront
+// Write a function called "removeFromFront".
+// Given an array, "removeFromFront" returns the given array with its first element removed.
+// Notes:* You should be familiar with the method 'shift'.
+
+function removeFromFront(arr) {
+  arr.shift();
+  return arr;
+}
